053c63476a047d40bf65f8850a9a05aca3e46cdd
UntoldTitan <untoldtitan3k@gmail.com>
2022-11-17 14:18:38 -0700

    Created Site

---
+++ src/App.svelte
@@ -0,0 +1,61 @@
+<div class="background">
+  <div class="center">
+    <h1>Shovelling Driveways</h1>
+  </div>
+  <div class="main center">
+    <div class="info">
+      <h2>Who?</h2>
+      <h3>Aj Davey</h3>
+      <p>62 Cougarstone Close SW, Calgary AB</p>
+    </div>
+    <div class="info">
+      <h2>What do I do?</h2>
+      <p>
+        I'm Aj, and I'm clearing driveways all throughout winter so you don't
+        have to! I'm able to remove packed snow, ice, and all other types of
+        snow that might be on your driveway!
+      </p>
+    </div>
+    <div class="info">
+      <h2>Times</h2>
+      <p>Monday - Thursday: 5:30pm - 9pm</p>
+      <p>Friday: 3pm - 9pm</p>
+      <p>Saturday - Sunday: 10am - 9pm</p>
+    </div>
+  </div>
+</div>
+
+<style>
+  .background {
+    background-color: rgb(88, 88, 88);
+    color: white;
+    width: 100%;
+    height: 100vh;
+  }
+  .center {
+    width: fit-content;
+    margin-left: auto;
+    margin-right: auto;
+  }
+  h1 {
+    margin: 0;
+    padding-top: 15px;
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 75px;
+  }
+  .info {
+    background-color: rgb(59, 59, 59);
+    border-radius: 35px;
+    margin-left: 10px;
+    margin-right: 10px;
+    width: 500px;
+    height: 300px;
+    text-align: center;
+  }
+
+  .main {
+    font-family: Arial, Helvetica, sans-serif;
+    margin-top: 50px;
+    display: flex;
+  }
+</style>
